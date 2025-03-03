efe3985f1fa8a0c2414232d35c319e1d639b44dc
kalashkaushal123 <kalashkaushal664@gmail.com>
2025-03-03 19:24:10 +0530

    home page

--- wedding/static/js/home.js
+++ wedding/static/js/home.js
@@ -1,33 +1,46 @@
 let slideIndex = 1;
 showslide(slideIndex);
 
-function changeSlide(n){
+// Automatically change slides every 2 seconds
+setInterval(() => {
+    changeSlide(1); // Move to the next slide
+}, 2000);
+
+function changeSlide(n) {
     showslide(slideIndex += n);
 }
 
-function currentSlide(n){
+function currentSlide(n) {
     slideIndex = n;
     showslide(slideIndex);
-}   
+}
 
-function showslide(n){
-    let i;
+function showslide(n) {
     let slides = document.getElementsByClassName("review-slide");
 
-    if(n > slides.length){
+    if (n > slides.length) {
         slideIndex = 1;
     }
     
-    if(n < 1){
+    if (n < 1) {
         slideIndex = slides.length;
     }
 
-    for(i = 0; i < slides.length; i++){
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
-    slides[slideIndex-1].style.display = "block"
+    slides[slideIndex - 1].style.display = "block";
 }
 
-console.log("Hello");
 
+
+document.addEventListener("DOMContentLoaded", function () {
+    let textElement = document.getElementById("home_text");
+    let words = textElement.innerText.split(" "); // Split text into words
+    console.log(words);
+    
+    if (words.length > 20) {
+      textElement.innerText = words.slice(0, 20).join(" ") + "..."; // Keep first 150 words
+    }
+  });
